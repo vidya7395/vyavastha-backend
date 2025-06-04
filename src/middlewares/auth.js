027ea6fe301ba2b5718563e0038b2e2277d43fa9
Vidhya Satpute <vidhya7395@gmail.com>
2025-06-04 16:13:12 +0530

    added config

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -13,23 +13,28 @@ const adminAuth = (req, res, next) => {
 
 const userAuth = async (req, res, next) => {
   try {
-    const token = await req.cookies?.token;
+    const token = req.cookies?.token;
     if (!token) {
-      return res.status(401).send('Token ISSUE Please login again!');
+      return res
+        .status(401)
+        .json({ message: 'Token missing. Please login again!' });
     }
     const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
     const { _id } = decodedObj;
     if (!_id) {
-      throw new Error('Please login again!');
+      return res
+        .status(401)
+        .json({ message: 'Invalid token. Please login again!' });
     }
     const user = await Users.findById(_id);
     if (!user) {
-      throw new Error('User not found!');
+      return res.status(404).json({ message: 'User not found!' });
     }
     req.user = user;
     next();
   } catch (error) {
-    res.status(400).send('ERROR:' + error.message);
+    console.error('userAuth error:', error.message);
+    res.status(400).json({ message: 'Authentication error: ' + error.message });
   }
 };
 
